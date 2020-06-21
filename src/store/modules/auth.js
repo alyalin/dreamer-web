5fa51f8012535540dd97049d16831544a5a59fc8
Alexander Lyalin <occvlt@yandex.ru>
2020-06-21 20:10:19 +0400

    :zap: remove setToken action (moved to main.js)

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -1,9 +1,10 @@
+import Cookies from 'js-cookie';
+
 const authModule = {
   namespaced: true,
   state: () => ({
     isAuth: false,
     isRegSucceed: false,
-    token: null,
   }),
   mutations: {
     SET_IS_REG_SUCCEED(state, payload) {
@@ -11,9 +12,6 @@ const authModule = {
     },
     SET_AUTHENTICATED_STATE(state, payload) {
       state.isAuth = payload;
-    },
-    SET_TOKEN(state, payload) {
-      state.token = payload;
     },
   },
   actions: {
@@ -25,9 +23,15 @@ const authModule = {
           { withCredentials: true },
         );
         const jwt = token.data.access_token;
+
+        Cookies.set('jwt', jwt, {
+          expires: new Date(jwtExpiresIn * 1000),
+          sameSite: 'strict',
+          secure: process.env.NODE_ENV === 'production',
+        });
+
         this.$router.push('/');
 
-        commit('SET_TOKEN', jwt);
         commit('SET_AUTHENTICATED_STATE', !!jwt);
 
         // await this.app.$stopJwtRefresh()
@@ -57,36 +61,13 @@ const authModule = {
         const req = await this.$axios.get('/auth/logout', {
           withCredentials: true,
         });
-        commit('SET_TOKEN', null);
         commit('SET_AUTHENTICATED_STATE', false);
-        // this.app.$stopJwtRefresh();
-        // this.$axios.setToken(false)
         this.$router.push('/');
       } catch (e) {
-        commit('SET_TOKEN', null);
         commit('SET_AUTHENTICATED_STATE', false);
         console.warn(e);
       }
-    },
-
-    async getToken({ commit }) {
-      try {
-        const token = await this.$axios.post(
-          '/auth/refresh-token',
-          {},
-          { withCredentials: true },
-        );
-        commit('SET_TOKEN', token.data.access_token);
-        commit('SET_AUTHENTICATED_STATE', !!token.data.access_token);
-      } catch (error) {
-        commit('SET_TOKEN', null);
-        commit('SET_AUTHENTICATED_STATE', false);
-        // this.$axios.setToken(false)
-        // if (app.$stopJwtRefresh) {
-        //   app.$stopJwtRefresh()
-        // }
-      }
-    },
+    }
   },
   getters: {
     isAuth: (state) => state.isAuth,
