b4279e6cbb4a352e52fd9f40e522e89e42b4a39f
Alexander Lyalin <occvlt@yandex.ru>
2020-06-21 20:10:41 +0400

    :zap: add jwt extract function

---
+++ src/utils/jwt-decode.js
@@ -0,0 +1,9 @@
+export function parseJwt(token) {
+  let base64Url = token.split('.')[1]; // token you get
+  let base64 = base64Url.replace('-', '+').replace('_', '/');
+  let decodedData = JSON.parse(
+    Buffer.from(base64, 'base64').toString('binary'),
+  );
+
+  return decodedData;
+}
